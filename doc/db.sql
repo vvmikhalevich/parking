8a46d8617575fa87b0f498605d7fc39264ef6616
Miravi-ПК\Miravi <v.v.mikhalevich@gmail.com>
2019-01-12 22:29:03 +0300

    old version

--- doc/db.sql
+++ doc/db.sql
@@ -27,7 +27,6 @@ CREATE TABLE "car" (
 	"id" serial NOT NULL,
 	"model_id" integer NOT NULL,
 	"number" character varying NOT NULL,
-	"user_account_id" integer NOT NULL,
 	"foto_id" integer NOT NULL,
 	"created" TIMESTAMP NOT NULL,
 	"updated" TIMESTAMP NOT NULL,
@@ -110,7 +109,7 @@ CREATE TABLE "transaction" (
 
 CREATE TABLE "place_owner" (
 	"id" serial NOT NULL,
-	"user_account_id" integer NOT NULL,
+	"client_id" integer NOT NULL,
 	"place_id" integer NOT NULL,
 	"created" TIMESTAMP NOT NULL,
 	"updated" TIMESTAMP NOT NULL,
@@ -152,7 +151,7 @@ CREATE TABLE "place" (
 
 
 
-CREATE TABLE "parking copy" (
+CREATE TABLE "event" (
 	"id" serial NOT NULL,
 	"car_id" integer NOT NULL,
 	"place_id" integer NOT NULL,
@@ -160,7 +159,7 @@ CREATE TABLE "parking copy" (
 	"time_end" TIMESTAMP NOT NULL,
 	"created" TIMESTAMP NOT NULL,
 	"updated" TIMESTAMP NOT NULL,
-	CONSTRAINT parking copy_pk PRIMARY KEY ("id")
+	CONSTRAINT event_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -171,8 +170,7 @@ CREATE TABLE "parking copy" (
 ALTER TABLE "model" ADD CONSTRAINT "model_fk0" FOREIGN KEY ("brand_id") REFERENCES "brand"("id");
 
 ALTER TABLE "car" ADD CONSTRAINT "car_fk0" FOREIGN KEY ("model_id") REFERENCES "model"("id");
-ALTER TABLE "car" ADD CONSTRAINT "car_fk1" FOREIGN KEY ("user_account_id") REFERENCES "user_account"("id");
-ALTER TABLE "car" ADD CONSTRAINT "car_fk2" FOREIGN KEY ("foto_id") REFERENCES "foto"("id");
+ALTER TABLE "car" ADD CONSTRAINT "car_fk1" FOREIGN KEY ("foto_id") REFERENCES "foto"("id");
 
 
 
@@ -183,13 +181,14 @@ ALTER TABLE "client" ADD CONSTRAINT "client_fk2" FOREIGN KEY ("tariff_id") REFER
 
 ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk0" FOREIGN KEY ("client_id") REFERENCES "client"("id");
 
-ALTER TABLE "place_owner" ADD CONSTRAINT "place_owner_fk0" FOREIGN KEY ("user_account_id") REFERENCES "user_account"("id");
+ALTER TABLE "place_owner" ADD CONSTRAINT "place_owner_fk0" FOREIGN KEY ("client_id") REFERENCES "client"("id");
 ALTER TABLE "place_owner" ADD CONSTRAINT "place_owner_fk1" FOREIGN KEY ("place_id") REFERENCES "place"("id");
 
 
 ALTER TABLE "place" ADD CONSTRAINT "place_fk0" FOREIGN KEY ("parking_id") REFERENCES "parking"("id");
 ALTER TABLE "place" ADD CONSTRAINT "place_fk1" FOREIGN KEY ("car_id") REFERENCES "car"("id");
 
-ALTER TABLE "parking copy" ADD CONSTRAINT "parking copy_fk0" FOREIGN KEY ("car_id") REFERENCES "car"("id");
-ALTER TABLE "parking copy" ADD CONSTRAINT "parking copy_fk1" FOREIGN KEY ("place_id") REFERENCES "place"("id");
+ALTER TABLE "event" ADD CONSTRAINT "event_fk0" FOREIGN KEY ("car_id") REFERENCES "car"("id");
+ALTER TABLE "event" ADD CONSTRAINT "event_fk1" FOREIGN KEY ("place_id") REFERENCES "place"("id");
+
 
