7200d0ade68221f13e4716baaec77456365b6c25
Viktar_Mikhalevich <v.v.mikhalevich@gmail.com>
2019-10-31 19:50:34 +0300

    Working project!!!(Spring)

--- service/src/test/java/com/itacademy/jd2/vvm/parking/service/impl/BrandServiceTest.java
+++ service/src/test/java/com/itacademy/jd2/vvm/parking/service/impl/BrandServiceTest.java
@@ -5,82 +5,79 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
-
+import com.itacademy.jd2.vvm.parking.dao.api.entity.table.IBrand;
 import org.junit.jupiter.api.Test;
 
-import com.itacademy.jd2.vvm.parking.dao.api.entity.table.IBrand;
+import java.util.List;
 
 public class BrandServiceTest extends AbstractTest {
 
-	@Test
-	public void testCreate() {
-		final IBrand entity = saveNewBrand();
+    @Test
+    public void testCreate() {
+        final IBrand entity = saveNewBrand();
 
-		final IBrand entityFromDb = brandService.get(entity.getId());
+        final IBrand entityFromDb = brandService.get(entity.getId());
 
-		assertNotNull(entityFromDb);
-		assertEquals(entity.getName(), entityFromDb.getName());
-		assertNotNull(entityFromDb.getId());
-		assertNotNull(entityFromDb.getCreated());
-		assertNotNull(entityFromDb.getUpdated());
-		assertTrue(entityFromDb.getCreated().equals(entityFromDb.getUpdated()));
-	}
+        assertNotNull(entityFromDb);
+        assertEquals(entity.getName(), entityFromDb.getName());
+        assertNotNull(entityFromDb.getId());
+        assertNotNull(entityFromDb.getCreated());
+        assertNotNull(entityFromDb.getUpdated());
+        assertTrue(entityFromDb.getCreated().equals(entityFromDb.getUpdated()));
+    }
 
-	
 
-	@Test
-	public void testUpdate() throws InterruptedException {
-		final IBrand entity = saveNewBrand();
+    @Test
+    public void testUpdate() throws InterruptedException {
+        final IBrand entity = saveNewBrand();
 
-		String newName = entity.getName() + "_updated";
-		entity.setName(newName);
-		Thread.sleep(2000);
-		brandService.save(entity);
+        String newName = entity.getName() + "_updated";
+        entity.setName(newName);
+        Thread.sleep(2000);
+        brandService.save(entity);
+        final IBrand entityFromDb = brandService.get(entity.getId());
 
-		final IBrand entityFromDb = brandService.get(entity.getId());
+        assertNotNull(entityFromDb);
+        assertEquals(newName, entityFromDb.getName());
+        assertNotNull(entityFromDb.getId());
+        assertNotNull(entityFromDb.getCreated());
+        assertNotNull(entityFromDb.getUpdated());
+        assertEquals(entity.getCreated(), entityFromDb.getCreated());
+        assertTrue(entityFromDb.getUpdated().after(entity.getCreated()));
+    }
 
-		assertNotNull(entityFromDb);
-		assertEquals(newName, entityFromDb.getName());
-		assertNotNull(entityFromDb.getId());
-		assertNotNull(entityFromDb.getCreated());
-		assertNotNull(entityFromDb.getUpdated());
-		assertEquals(entity.getCreated(), entityFromDb.getCreated());
-		assertTrue(entityFromDb.getUpdated().after(entity.getCreated()));
-	}
+    @Test
+    public void testGetAll() {
+        final int intialCount = brandService.getAll().size();
 
-	@Test
-	public void testGetAll() {
-		final int intialCount = brandService.getAll().size();
+        final int randomObjectsCount = getRandomObjectsCount();
+        for (int i = 0; i < randomObjectsCount; i++) {
+            saveNewBrand();
+        }
 
-		final int randomObjectsCount = getRandomObjectsCount();
-		for (int i = 0; i < randomObjectsCount; i++) {
-			saveNewBrand();
-		}
+        final List<IBrand> allEntities = brandService.getAll();
 
-		final List<IBrand> allEntities = brandService.getAll();
+        for (final IBrand entityFromDb : allEntities) {
+            assertNotNull(entityFromDb.getName());
+            assertNotNull(entityFromDb.getId());
+            assertNotNull(entityFromDb.getCreated());
+            assertNotNull(entityFromDb.getUpdated());
+        }
 
-		for (final IBrand entityFromDb : allEntities) {
-			assertNotNull(entityFromDb.getName());
-			assertNotNull(entityFromDb.getId());
-			assertNotNull(entityFromDb.getCreated());
-			assertNotNull(entityFromDb.getUpdated());
-		}
+        assertEquals(randomObjectsCount + intialCount, allEntities.size());
+    }
 
-		assertEquals(randomObjectsCount + intialCount, allEntities.size());
-	}
+    @Test
+    public void testDelete() {
+        final IBrand entity = saveNewBrand();
+        brandService.delete(entity.getId());
+        assertNull(brandService.get(entity.getId()));
+    }
 
-	@Test
-	public void testDelete() {
-		final IBrand entity = saveNewBrand();
-		brandService.delete(entity.getId());
-		assertNull(brandService.get(entity.getId()));
-	}
-
-	@Test
-	public void testDeleteAll() {
-		saveNewBrand();
-		brandService.deleteAll();
-		assertEquals(0, brandService.getAll().size());
-	}
+    @Test
+    public void testDeleteAll() {
+        saveNewBrand();
+        brandService.deleteAll();
+        assertEquals(0, brandService.getAll().size());
+    }
 }
