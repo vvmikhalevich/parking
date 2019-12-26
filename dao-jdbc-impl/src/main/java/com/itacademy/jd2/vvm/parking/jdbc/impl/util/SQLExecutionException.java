df8e2623b7e496ffaa6ae98e1bbd470477431655
Viktar_Mikhalevich <v.v.mikhalevich@gmail.com>
2019-12-26 20:53:04 +0300

    Add some code for tomcat!!!

--- dao-jdbc-impl/src/main/java/com/itacademy/jd2/vvm/parking/jdbc/impl/util/SQLExecutionException.java
+++
@@ -1,9 +0,0 @@
-package com.itacademy.jd2.vvm.parking.jdbc.impl.util;
-
-public class SQLExecutionException extends RuntimeException {
-
-    public SQLExecutionException(final Exception cause) {
-        super(cause);
-    }
-
-}
